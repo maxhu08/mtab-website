81d0a702acf2b40138f18d000ec559b5b473b277
maxhu08 <contact@maxhu.dev>
2024-09-26 18:31:52 -0500

    add more stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
+import { FancyImage } from "~/components/fancy-image";
+
 const Page = () => {
   return (
     <div className="min-h-screen">
-      <div className="grid place-items-center py-20">
-        <h1 className="text-white">the modern new tab extension</h1>
+      <div className="grid grid-flow-row gap-4 py-20">
+        <div className="grid place-items-center py-10 lg:py-20">
+          <h1 className="text-white text-2xl md:text-4xl lg:text-6xl text-center">
+            the <span className="text-emerald-500">modern</span> new tab extension
+          </h1>
+          <div className="grid grid-cols-2 gap-4 pt-10">
+            <a
+              href="https://chromewebstore.google.com/detail/mtab/fdaphilojaklgkoocegabckfanjoacjg"
+              target="_blank"
+              className="cursor-pointer">
+              <div className="w-[10rem] lg:w-[14rem]">
+                <FancyImage src="/assets/download-chrome.svg" />
+              </div>
+            </a>
+            <a
+              href="https://addons.mozilla.org/en-US/firefox/addon/mtab"
+              target="_blank"
+              className="cursor-pointer">
+              <div className="w-[10rem] lg:w-[14rem]">
+                <FancyImage src="/assets/download-firefox.svg" />
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="grid place-items-center">
+          <div className="w-[80%] border-sky-500 border-2 rounded-lg">
+            <FancyImage src="/assets/showcase.png" />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ components/fancy-image.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { FC } from "react";
+
+interface FancyImageProps {
+  src: string;
+  alt?: string;
+}
+
+export const FancyImage: FC<FancyImageProps> = ({ src, alt }) => {
+  return (
+    <div className="my-2 rounded-md md:rounded-lg overflow-hidden relative w-full h-max">
+      <Image
+        src={src}
+        alt={alt ?? ""}
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full h-auto"
+      />
+    </div>
+  );
+};
