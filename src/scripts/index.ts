79833743fb279f4cdbeb50158536d317a4a843a4
maxhu08 <contact@maxhu.dev>
2026-02-03 21:09:49 -0600

    add rotating message

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -31,6 +31,49 @@
 })();
 
 (() => {
+  const el = document.getElementById("rotating-text");
+  if (!el) return;
+
+  const messages = [
+    '🧠 the <span class="text-emerald-500">modern</span> new tab',
+    '🔒 <span class="text-lime-500">privacy</span> focused',
+    '🎨 customized to <span class="text-pink-500">you</span>',
+    '⚙️ <span class="text-yellow-500">endless</span> customizations',
+    '⚙️ <span class="text-teal-300">qol</span> features'
+  ];
+
+  let i = 0;
+
+  const show = () => {
+    el.innerHTML = messages[i];
+    el.classList.remove("opacity-0", "translate-y-4");
+    el.classList.add("opacity-100", "translate-y-0");
+  };
+
+  const hide = (cb: () => void) => {
+    el.classList.remove("opacity-100", "translate-y-0");
+    el.classList.add("opacity-0", "translate-y-4");
+
+    const onEnd = (e: TransitionEvent) => {
+      if (e.target !== el) return;
+      el.removeEventListener("transitionend", onEnd);
+      cb();
+    };
+
+    el.addEventListener("transitionend", onEnd);
+  };
+
+  requestAnimationFrame(show);
+
+  setInterval(() => {
+    hide(() => {
+      i = (i + 1) % messages.length;
+      requestAnimationFrame(show);
+    });
+  }, 3000);
+})();
+
+(() => {
   const el = document.getElementById("preview");
   if (!el) return;
 
